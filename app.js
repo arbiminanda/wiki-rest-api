50994404c54dfce1a46ee44f747edf0e1baf573d
Muhammad Arbi Minanda <arbiminanda19@gmail.com>
2023-04-01 22:04:48 +0700

    set DB schema & collection model

--- app.js
+++ app.js
@@ -4,12 +4,20 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 
 const app = express();
+
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use("view engine", ejs);
 app.use(express.static("public"));
+
 mongoose.connect("mongodb://localhost:27017/express", {
   useNewUrlParser: true,
 });
+const articleSchema = {
+  title: String,
+  content: String,
+};
+const Article = mongoose.model("Article", articleSchema);
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
