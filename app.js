1358e634994a3b3cfef4f28284d4aa6fc386f74a
Muhammad Arbi Minanda <arbiminanda19@gmail.com>
2023-04-01 21:56:16 +0700

    set ejs & mongodb connection

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use("view engine", ejs);
+app.use(express.static("public"));
+mongoose.connect("mongodb://localhost:27017/express", {
+  useNewUrlParser: true,
+});
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
